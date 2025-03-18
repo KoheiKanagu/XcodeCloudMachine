a48aa7d6305ac6fe05c90e3717dabefcb85c18fd
KoheiKanagu <kanagu@kingu.dev>
2025-03-18 23:31:35 +0900

    Initial Commit

---
+++ XcodeCloudMachine/XcodeCloudMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudMachineApp.swift
+//  XcodeCloudMachine
+//
+//  Created by kingu on 2025/03/18.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
